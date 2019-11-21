89cecf4807fa38e7fce3149b7de01ec7488c752a
Andrés Sánchez <andres_sa.97@hotmail.com>
2019-11-21 17:34:02 -0600

    Changed test fibonacci code

--- test.js
+++ test.js
@@ -2,17 +2,40 @@ const spook = require('./spook');
 const VM = require('./SpookVM');
 
 var testCode = `
-def a : decimal[3]
+fun fibonacci(n:decimal)
+	def a, b, temp : decimal
+	a = 1
+	b = 0
+	while n>=0 do
+		temp = a
+		a = a+b
+		b = temp
+		n = n-1
+	end
+	return b
+end
 
 fun start()
-	a[0] = 1
-	a[1] = 2
-	a[2] = a[0] + a[1]
+	print(fibonacci(9))
 end
 `
+
+function fibonacci(num){
+	var a = 1, b = 0, temp;
+	while (num >= 0){
+	  	temp = a;
+	  	a = a + b;
+	  	b = temp;
+	 	num--;
+	}
+	return b;
+ }
 
 var res = spook.parse(testCode);
 
 var vm = new VM(res.quads, res.vars, res.funcs, res.const, res.temps);
 
-vm.doQuads();
+var out = vm.doQuads();
+
+console.log("CALC:", out)
+console.log("REAL: ", fibonacci(9))
